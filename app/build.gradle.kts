6fe3e39b7c4bed22bc3557396ab03a5e207b24aa
ismat27 <adegbiteismail27@gmail.com>
2024-01-09 12:12:23 +0100

    set up dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    id("androidx.navigation.safeargs.kotlin")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-kapt")
 }
 
 android {
@@ -41,6 +45,31 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+//    apply plugin: "kotlin-kapt"
+
+    //loading button
+    implementation("br.com.simplepass:loading-button-android:2.2.0")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.13.0")
+
+    //circular image
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+    //viewpager2 indicator
+    implementation("io.github.vejei.viewpagerindicator:viewpagerindicator:1.0.0-alpha.1")
+
+    //stepView
+    implementation("com.shuhart.stepview:stepview:1.5.1")
+
+    //Android Ktx
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+
+    //Dagger hilt
+    implementation("com.google.dagger:hilt-android:2.38.1")
+    kapt("com.google.dagger:hilt-compiler:2.38.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
