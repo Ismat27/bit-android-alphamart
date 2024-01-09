3094bd9cc79c2735c7c7c544fb3b6927bba44df4
ismat27 <adegbiteismail27@gmail.com>
2024-01-09 21:33:54 +0100

    set up content fragment

---
+++ app/src/main/java/com/example/estore/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package com.example.estore.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.estore.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register) {
+}
