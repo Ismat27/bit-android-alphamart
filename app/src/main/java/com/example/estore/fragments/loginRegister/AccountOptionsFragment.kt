692b37c8fce5fe820cfde0bfbab3018f17efae56
ismat27 <adegbiteismail27@gmail.com>
2024-01-11 17:46:14 +0100

    update auth flow

--- app/src/main/java/com/example/estore/fragments/loginRegister/AccountOptionsFragment.kt
+++ app/src/main/java/com/example/estore/fragments/loginRegister/AccountOptionsFragment.kt
@@ -1,7 +1,35 @@
 package com.example.estore.fragments.loginRegister
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.estore.R
+import com.example.estore.databinding.FragmentAccountOptionsBinding
 
-class AccountOptionsFragment: Fragment(R.layout.fragment_account_options) {
+class AccountOptionsFragment : Fragment(R.layout.fragment_account_options) {
+    private lateinit var binding: FragmentAccountOptionsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAccountOptionsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            buttonRegister.setOnClickListener {
+                findNavController().navigate(R.id.action_accountOptionsFragment_to_registerFragment)
+            }
+            buttonLogin.setOnClickListener {
+                findNavController().navigate(R.id.action_accountOptionsFragment_to_loginFragment)
+            }
+        }
+    }
 }
