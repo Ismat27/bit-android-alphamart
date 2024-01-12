fa4f849b530c90102bdf7fbc80574f7bfc70b0eb
ismat27 <adegbiteismail27@gmail.com>
2024-01-12 14:40:07 +0100

    provide share preferences

--- app/src/main/java/com/example/estore/di/AppModule.kt
+++ app/src/main/java/com/example/estore/di/AppModule.kt
@@ -1,5 +1,9 @@
 package com.example.estore.di
 
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.example.estore.util.Constants.INTRO_FRAGMENT_SP
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
@@ -19,4 +23,8 @@ object AppModule {
     @Provides
     @Singleton
     fun provideFirebaseFirestore() = FirebaseFirestore.getInstance()
+
+    @Provides
+    fun provideIntroductionFragmentSP(application: Application): SharedPreferences =
+        application.getSharedPreferences(INTRO_FRAGMENT_SP, MODE_PRIVATE)
 }
