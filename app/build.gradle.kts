e3a1b25cdf558c9874ba93d0e2aa50f4e90e50fe
ismat27 <adegbiteismail27@gmail.com>
2024-01-12 14:54:53 +0100

    installed navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,14 +44,15 @@ android {
 }
 
 dependencies {
+    val daggerVersion = "2.50"
+    val navVersion = "2.7.6"
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-firestore:24.10.0")
-
-    val daggerVersion = "2.50"
+    implementation("com.google.firebase:firebase-auth:22.3.0")
 
     //loading button
     implementation("br.com.simplepass:loading-button-android:2.2.0")
@@ -68,12 +69,12 @@ dependencies {
     //stepView
     implementation("com.shuhart.stepview:stepview:1.5.1")
 
-    //Android Ktx
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     //Dagger hilt
     implementation("com.google.dagger:hilt-android:$daggerVersion")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
     kapt("com.google.dagger:hilt-android-compiler:$daggerVersion")
 
     testImplementation("junit:junit:4.13.2")
