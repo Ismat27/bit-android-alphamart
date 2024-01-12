8e8c37f216bdf95016a841eb65c7f66a94554626
ismat27 <adegbiteismail27@gmail.com>
2024-01-12 12:56:38 +0100

    add constants

--- app/src/main/java/com/example/estore/util/Constants.kt
+++ app/src/main/java/com/example/estore/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val USER_COLLECTION = "users"
     const val REGISTER_TAG = "RegisterTag"
     const val LOGIN_TAG = "LoginTag"
+    const val INTRO_FRAGMENT_SP = "IntroFragmentSP"
+    const val INTRO_BUTTON_KEY = "IntroButton"
 }
