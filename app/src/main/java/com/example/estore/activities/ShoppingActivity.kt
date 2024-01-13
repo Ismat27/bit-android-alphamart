150cab8e54a15242209097456916a5b97753f8e5
ismat27 <adegbiteismail27@gmail.com>
2024-01-13 11:28:32 +0100

    set up nav graph and components for shopping activity

--- app/src/main/java/com/example/estore/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/estore/activities/ShoppingActivity.kt
@@ -1,12 +1,24 @@
 package com.example.estore.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.estore.R
+import com.example.estore.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityShoppingBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentShoppingHost) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/estore/fragments/shopping/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.estore.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.estore.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+}
